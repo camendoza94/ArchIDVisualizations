43e125ea0db49a4f9f21f99e103a30fb90d421aa
Camilo Mendoza <camendoza94@gmail.com>
2019-05-07 11:24:36 -0500

    Script working in every project.

--- server/server.js
+++ server/server.js
@@ -1,38 +1,53 @@
 const glob = require('glob');
 const initial = require('./architecture');
-const fs = require('fs');
 const axios = require('axios');
 
 let architecture = JSON.parse(JSON.stringify(initial));
 let mod = {};
+let data = {};
 
-const getFiles = (src) => {
+const colors = ["#ede0da", "#cca92a", "#d66228", "#d62445", "#a0001d", "#d62445"];
+
+function getPath(s) {
+    let parts = s.split("/");
+    let path = "";
+    for (let i = 6; i >= 0; i--) {
+        if (i === 0)
+            path += parts[parts.length - 1 - i];
+        else
+            path += parts[parts.length - 1 - i] + "/";
+    }
+    return path;
+
+}
+
+const getFiles = async (src) => {
+    let currentData = data.find(project => project.name === require('path').basename(process.cwd())).data[0];
     architecture.children.forEach((child) => {
         mod = child;
         child.pattern.forEach((pattern) => {
             let res = glob.sync(src + pattern);
             let newRes = res.map(file => {
+                let found = currentData.files.find(f => f.name === getPath(file));
+                let sum = found ? found.issues.reduce((a, b) => a + b, 0) : 0;
                 return {
                     name: file.split("/")[file.split("/").length - 1],
-                    size: 1,
-                    color: "#d62445"
+                    size: sum + 1,
+                    color: colors[sum % 5]
                 }
             });
             architecture.children.find(child => child.name === mod.name).children.push(...newRes);
-            fs.writeFile("src/architecture2.json", JSON.stringify(architecture), (err) => {
-                if (err) {
-                    console.log(err);
-                }
-            });
-        })
+        });
     });
 };
 
 const getIssues = () => {
-    axios.get("https://archtoringbd.herokuapp.com/files").then((response) => {
-        getFiles('C:/Data/Github/s1_Eventos_201910');
-        let files = response.data;
-
+    axios.get("https://archtoringbd.herokuapp.com/files").then(async (response) => {
+        data = response.data;
+        await getFiles('./');
+        axios.put(`https://archtoringbd.herokuapp.com/architecture/${require('path').basename(process.cwd())}`, architecture).then((response) => {
+            console.log(response);
+        })
     })
 };
 
