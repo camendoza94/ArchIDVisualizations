c3812a5637157acef018a9f6d1b091b177384a05
Camilo Mendoza <camendoza94@gmail.com>
2019-10-12 18:44:20 -0500

    Removed test code

--- src/components/CodeView.js
+++ src/components/CodeView.js
@@ -60,22 +60,6 @@ class CodeView extends Component {
                         }}>
                         {contents}
                     </SyntaxHighlighter> : ""}
-                <button type="button" className="btn btn-secondary" data-toggle="tooltip" data-placement="top"
-                        title="Tooltip on top">
-                    Tooltip on top
-                </button>
-                <button type="button" className="btn btn-secondary" data-toggle="tooltip" data-placement="right"
-                        title="Tooltip on right">
-                    Tooltip on right
-                </button>
-                <button type="button" className="btn btn-secondary" data-toggle="tooltip" data-placement="bottom"
-                        title="Tooltip on bottom">
-                    Tooltip on bottom
-                </button>
-                <button type="button" className="btn btn-secondary" data-toggle="tooltip" data-placement="left"
-                        title="Tooltip on left">
-                    Tooltip on left
-                </button>
             </Fragment>
         )
     }
